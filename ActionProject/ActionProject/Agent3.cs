6acf8845423b76f5a5ec4a3508eac291069ce2a6
Shellyaz26 <TheLittleCitizen4@gmail.com>
2020-08-26 17:00:17 +0300

    new project

---
+++ ActionProject/ActionProject/Agent3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActionProject 
+{
+    public class Agent3 : InterfaceAgent
+    {
+        public void abouttheproduct(string description)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void getstartcost(int num)
+        {
+
+        }
+
+        public void nameofproduct(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Participantion()
+        {
+            Random _random7 = new Random();
+            double num7 = _random7.NextDouble();
+            return num7 > 0.5;
+        }
+
+        public void getsellgap(int number)
+        {
+
+        }
+
+        public bool Newprice(string sentence)
+        {
+            Random _random8 = new Random();
+            double num8 = _random8.NextDouble();
+            return num8 > 0.5;
+        }
+
+    }
+     
+}
